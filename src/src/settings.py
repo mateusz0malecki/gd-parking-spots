3153d7c9bec8f2ab3f0e6126fc435526df8f3174
Mateusz Malecki <mmalecki0620@gmail.com>
2025-08-29 14:29:03 +0200

    infra: add dockerfile

--- src/src/settings.py
+++ src/src/settings.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(dotenv_path=BASE_DIR / ".env")
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
